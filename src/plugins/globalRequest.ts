33436773c28b4ce693196a51f0f3490bf82c0cf4
Fetters <997167305@qq.com>
2024-11-14 17:02:33 +0800

    部署

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -11,6 +11,7 @@ import { extend } from 'umi-request';
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
+  prefix: process.env.NODE_ENV === 'production'? 'http://user-backend.code-nav.cn': undefined,
   // requestType: 'form',
 });
 
